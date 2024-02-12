a683402077edde6c512bfa5451dcaffcc44f02c7
Michael-she <mgshepstone@gmail.com>
2024-02-12 11:31:10 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// Using ES Module syntax for Next.js configuration
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/api/:slug*', // Adjust the path as necessary
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-store',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  // Export the configuration using ES Module syntax
+  export default nextConfig;
+  
